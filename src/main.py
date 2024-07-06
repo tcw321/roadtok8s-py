c785ebb43de011c244879d09b574465c0decd03f
Timothy C. Wright <tcw321@gmail.com>
2024-07-06 10:29:45 -0400

    initial project

---
+++ src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_index():
+	"""
+	Return a Python Dictionary that supports JSON serialization
+	"""
+	return {"Hello": "World"}
+
+@app.get("/api/v1/hello-world")
+def read_hello_world():
+	"""
+	Return an API-like response.
+	"""
+	return {"what":"road", "where":"kubernetes", "version": "v1"}
